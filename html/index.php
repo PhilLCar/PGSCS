6f6652d93938e208d0fbf1730931136609af529a
Philippe Caron <philippe-caron@hotmail.com>
2022-02-17 11:55:27 -0400

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>PGSCS Dashboard</title>
+  <script type="text/javascript"  src="js/script.js"></script>
+  <link   type="text/css"        href="css/style.css"  rel="stylesheet"/>
+ </head>
+ <body>
+  <h1>POWER GENERATION SUPERVISORY CONTROL SYSTEM</h1>
+ </body>
+</html>
